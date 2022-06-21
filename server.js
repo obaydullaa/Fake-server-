2f0fd37726d99bd497991e16cc5030b2d594aa17
obaydullaa <cr7obaydul@gmail.com>
2022-06-21 12:13:58 +0600

    Fake server complete

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const PORT = process.env.PORT || 4000;
+
+
+server.listen(PORT, () => {
+  console.log('JSON Server is running')
+})
